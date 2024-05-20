1d409fc41ec830f6c1bf0f02383edd6011853654
Miriam-Sanvito <sanvitmimi@gmail.com>
2024-05-20 12:52:48 +0200

    cella

---
+++ cella.py
@@ -0,0 +1,7 @@
+import pygame
+class Cella:
+    def __init__(self, i, j, pos, size) -> None:
+        self.i = i
+        self.j = j
+        self.pos = pos 
+        self.size = size
---
+++ tavolo.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Tavolo:
+    def __init__(self, display, ncelle_righe, ncelle_colonne) -> None:
+        self.display = display
+        self.ncelle_righe = ncelle_righe
+        self.ncelle_colonne
+        
+    
+        for i in range(ncelle_righe):
+            for j in range(ncelle_colonne):
+                pygame.draw(cella)
+              
