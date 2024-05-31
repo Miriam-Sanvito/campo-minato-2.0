2f79d47bb5bf55cd90ccaacd6f9cb9fdf0acdf70
giorgiaagianni <giorgiagianni07@gmail.com>
2024-05-31 16:34:55 +0200

    schermata iniziale

--- main.py
+++ main.py
@@ -20,20 +20,42 @@ altezza_cella = (altezza_schermo/numero_righe)
 larghezza_cella = (larghezza_schermo/numero_colonne)
 
 #schermata iniziale
-x_start = 150
-y_start = 300
-larghezza_start = 200
-altezza_start = 100
+x_start = (larghezza_schermo/2-65)
+y_start = (altezza_schermo/2+32)
+larghezza_start = 150
+altezza_start = 60
 
 clock = pygame.time.Clock()
 fps = 60
 
+class Bottone:
+    def __init__(self, screen, pos, size):
+        self.screen = screen
+        self.pos = pos 
+        self.size = size
+        self.image = pygame.Surface(size)
+        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
+        self.cliccato = False
+        self.colore_sfondo = (255, 255, 255)  # Bianco per lo sfondo
+        self.colore_bordo = (255, 111, 0)
+        
+        font = pygame.font.Font("/Users/sofiagianni/campo minato /ArchivoBlack-Regular.ttf", 50)
+        self.testo = font.render("play", (0.4), (255, 111, 0))
+        self.testo_rect = self.testo.get_rect(center=(size[0] // 2, size[1] // 2))
+    
+    def draw(self):
+        self.image.fill(self.colore_sfondo)  
+        pygame.draw.rect(self.image, self.colore_bordo, self.image.get_rect(), 5) 
+        self.image.blit(self.testo, self.testo_rect)
+        self.screen.blit(self.image, self.rect)
 
 def schermatainiziale():
     schermo.fill ((0,0,0))
-    rect = pygame.Rect(150, 300, 200, 100)
-    pygame.draw.rect(schermo, (255, 255, 255), rect)  
-    pygame.display.flip()
+    immagine = pygame.image.load('inizialecampominato.jpeg')
+    immagine = pygame.transform.scale(immagine, (larghezza_schermo, altezza_schermo))
+    schermo.blit(immagine, (0,0))
+    bottone = Bottone(schermo, (x_start, y_start), (larghezza_start, altezza_start))
+    bottone.draw()
     
 
 tavolo = Tavolo(schermo, (0, 75), (larghezza_schermo, altezza_schermo-75))
